a64731bcaa14aa60044e5a4e889755a6d6ff600a
saispandana7935 <saispandana7935@gmail.com>
2020-12-27 22:21:45 +0530

    ADD.added sif tatement to cdisplay who is win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,23 +1,40 @@
 #! /bin/bash -x
-read -p "Enter Number Of Flips" Num
+#read -p "Enter Number Of Flips" Num
 isHeads=1
 isTails=0
 headCount=0
 tailCount=0
 
-for((i=1; i<=$Num; i++))
+while [ true ]
 do
 	flipCoin=$((RANDOM%2))
 	case $flipCoin in
 		$isHeads)
 			echo "Heads"
 			((headCount++))
+			if [ $headCount -eq 21 ]
+			then
+				break
+			fi
 			 ;;
 		$isTails)
 			echo "Tails"
 			((tailCount++))
+			if [ $tailCount -eq 21 ]
+			then
+				break
+			fi
 			;;
 	esac
 done
 echo "Head_Count :" $headCount
 echo "Tail_Count :" $tailCount
+if [ $headCount -gt $tailCount ]
+then
+	echo "Heads win by " $(($headCount-$tailCount))
+elif [ $tailCount -gt $headCount ]
+then
+	echo "Tails win by " $(($tailCount-$headCount))
+else
+	echo "Tie"
+fi
