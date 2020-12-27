20e5446f49dbff064c7a4898cadb91583bf7cf6a
saispandana7935 <saispandana7935@gmail.com>
2020-12-27 22:02:57 +0530

    ADD.added statement to count heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,23 @@
 #! /bin/bash -x
-
+read -p "Enter Number Of Flips" Num
 isHeads=1
 isTails=0
-flipCoin=$((RANDOM%2))
-case $flipCoin in
-	$isHeads)
-		echo "Heads" ;;
-	$isTails)
-		echo "Tails" ;;
-esac
+headCount=0
+tailCount=0
+
+for((i=1; i<=$Num; i++))
+do
+	flipCoin=$((RANDOM%2))
+	case $flipCoin in
+		$isHeads)
+			echo "Heads"
+			((headCount++))
+			 ;;
+		$isTails)
+			echo "Tails"
+			((tailCount++))
+			;;
+	esac
+done
+echo "Head_Count :" $headCount
+echo "Tail_Count :" $tailCount
